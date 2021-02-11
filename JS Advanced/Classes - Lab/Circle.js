452598cf4d4759c7be7064e3ad3583296f23bb17
blagovest.yuriev.petrov <blagovest.yuriev.petrov@gmail.com>
2021-02-11 10:15:13 +0200

    Classes Lab

---
+++ JS Advanced/Classes - Lab/Circle.js
@@ -0,0 +1,14 @@
+class Circle {
+    constructor (radius){
+        this.radius = radius;
+    }
+    get diameter(){
+        return this.radius * 2;
+    }
+    set diameter(d){
+        this.radius = d/2;
+    }
+    get area(){
+        return Math.PI*(this.radius**2);
+    }
+}
