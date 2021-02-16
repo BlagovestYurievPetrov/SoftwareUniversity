0e036a6a03a62b3796c0136634e0c832bf0dd42f
blagovest.yuriev.petrov <blagovest.yuriev.petrov@gmail.com>
2021-02-16 22:24:25 +0200

    missing

--- JS Advanced/Exams/JS Advanced Exam 12.02.2021/PizzaPlace.test.js
+++
@@ -1,9 +0,0 @@
-const pizzUni = require('./PizzaPlace');
-const assert = require('Chai').assert;
-
-describe('Pizzuni Tests',()=>{
-    it('first test',()=>{
-        assert.equal(pizzUni.makeAnOrder({orderedPizza:'djob',orderedDrink:'matenica'}),'You just ordered djob and matenica.');
-    });
-
-})
