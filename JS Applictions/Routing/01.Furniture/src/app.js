5431ea6c623845ed213bb0bbb17211bf130eeee1
blagovest.yuriev.petrov <blagovest.yuriev.petrov@gmail.com>
2021-03-21 19:21:59 +0200

    Furniture app

--- JS Applictions/Routing/01.Furniture/src/app.js
+++ JS Applictions/Routing/01.Furniture/src/app.js
@@ -1,23 +1,48 @@
 import page from '../node_modules/page/page.mjs';
+import { render } from '../node_modules/lit-html/lit-html.js';
 
-import {dashboardPage} from './views/dashboard.js';
-import {detailsPage} from './views/details.js';
-import {createPage} from './views/create.js';
-import {editPage} from './views/edit.js';
-import {registerPage} from './views/register.js';
-import {loginPage} from './views/login.js';
-import {myPage} from './views/myFurniture.js';
+import { dashboardPage } from './views/dashboard.js';
+import { detailsPage } from './views/details.js';
+import { createPage } from './views/create.js';
+import { editPage } from './views/edit.js';
+import { registerPage } from './views/register.js';
+import { loginPage } from './views/login.js';
+import { myPage } from './views/myFurniture.js';
 
-import * as api from './api/api.js';
-api.settings.host = 'http://localhost:3030';
+import * as api from './api/data.js';
+import {logout} from './api/data.js'
 window.api = api;
 
-page('/',dashboardPage);
-page('/my-furniture',myPage);
-page('/details/:id',detailsPage);
-page('/create',createPage);
-page('/edit/:id',editPage);
-page('/register',registerPage);
-page('/login', loginPage);
+const main = document.querySelector('.container');
+page('/', decorateContext, dashboardPage);
+page('/my-furniture', decorateContext, myPage);
+page('/details/:id', decorateContext, detailsPage);
+page('/create', decorateContext, createPage);
+page('/edit/:id', decorateContext, editPage);
+page('/register', decorateContext, registerPage);
+page('/login', decorateContext, loginPage);
 
+document.getElementById('logoutBtn').addEventListener('click', async ()=>{
+    await logout();
+    page.redirect('/');
+    setUserNav();
+})
+setUserNav();
 page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, main);
+    ctx.setUserNav = setUserNav;
+    next();
+}
+
+function setUserNav(){
+    const userId = sessionStorage.getItem('userId');
+    if (userId != null){
+        document.getElementById('user').style.display = 'inline-block';
+        document.getElementById('guest').style.display = 'none';
+    } else {
+        document.getElementById('user').style.display = 'none';
+        document.getElementById('guest').style.display = 'inline-block';
+    }
+}
